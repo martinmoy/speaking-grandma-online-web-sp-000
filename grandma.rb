747d1bbe4c65251a40c7083c661cc366c4060e63
Martin Moy <martinmoy1991@gmail.com>
2020-10-03 09:35:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,14 +13,13 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif
     phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-
+    puts "NO, NOT SINCE 1938!"
   else
     phrase == !phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
 
   end
 end
